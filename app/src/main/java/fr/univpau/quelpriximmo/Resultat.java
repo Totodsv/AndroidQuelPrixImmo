b83b3fc1709d6a8bf27837787c52b3d54d61bc5b
VassDK <vassili65200@hotmail.fr>
2021-02-01 14:37:58 +0100

    SALUT BG

--- app/src/main/java/fr/univpau/quelpriximmo/Resultat.java
+++ app/src/main/java/fr/univpau/quelpriximmo/Resultat.java
@@ -22,6 +22,7 @@ import org.json.JSONObject;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -43,6 +44,12 @@ public class Resultat extends AppCompatActivity {
     String adresse;
     String date_mutation;
     int image;
+    ArrayList<Float>valeur = new ArrayList<Float>();
+    ArrayList<String> barEtendue = new ArrayList<>();
+    ArrayList<Integer> barNbValeur = new ArrayList<>();
+    ArrayList<HashMap> arlText = new ArrayList<>();
+    int barre1, barre2, barre3, barre4, barre5, barre6, barre7;
+    int nbBarre1=0, nbBarre2=0, nbBarre3=0, nbBarre4=0, nbBarre5=0, nbBarre6=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +65,7 @@ public class Resultat extends AppCompatActivity {
         listView.setAdapter(adapter);
 
         Bundle bundle = getIntent().getExtras();
-        ArrayList<HashMap> arlText = (ArrayList<HashMap>)bundle.getSerializable("Valeur");
+        arlText = (ArrayList<HashMap>)bundle.getSerializable("Valeur");
         // Add resultats
         if(arlText.size()>1){
             nombreResultats.setText(String.valueOf(arlText.size())+" biens ont été trouvés");
@@ -91,6 +98,7 @@ public class Resultat extends AppCompatActivity {
             //Log.i("Prix", String.valueOf(sansVirgule));
             String s = nf.format(Integer.parseInt((String.valueOf(sansVirgule))));
             valeur_fonciere=s+" €"; //Ajout du symbole € après le prix
+            valeur.add(Float.parseFloat((String) arlText.get(i).get("valeur_fonciere")));
 
             //valeur_fonciere=String.valueOf(arlText.get(i).get("valeur_fonciere"))+" €";
             type_local=String.valueOf(arlText.get(i).get("type_local"))+" / ";
@@ -100,13 +108,73 @@ public class Resultat extends AppCompatActivity {
             ListItem nouvelleMaison = new ListItem(image, valeur_fonciere,type_local,nombre_pieces_principales,adresse,date_mutation);
             this.adapter.add(nouvelleMaison);
         }
+
+        if(arlText.size()>0){
+            Collections.sort(valeur);
+            Log.i("CCCCCCCCCCC", String.valueOf(valeur));
+            int etendue = transfoZeroEtendue((float) ((transfoZeroPremier(valeur.get(0))+ transfoZerodernier(valeur.get(valeur.size()-1)))/6));
+            int barre=transfoZeroPremier(valeur.get(0)) ;;
+            barEtendue.add(String.valueOf(barre)+"€-"+String.valueOf(barre+etendue)+"€");
+            barre1 = transfoZeroPremier(valeur.get(0));
+            barre2 = transfoZeroPremier(valeur.get(0))+etendue;
+            barre3 = transfoZeroPremier(valeur.get(0))+etendue*2;
+            barre4 = transfoZeroPremier(valeur.get(0))+etendue*3;
+            barre5 = transfoZeroPremier(valeur.get(0))+etendue*4;
+            barre6 = transfoZeroPremier(valeur.get(0))+etendue*5;
+            for(int j=1; j<5;j++){
+                barre= barre + etendue;
+                Log.i("CCCCCCCCCCC", String.valueOf(barre));
+                barEtendue.add(String.valueOf(String.valueOf(barre)+"€-"+String.valueOf(barre+etendue)+"€"));
+            }
+            barEtendue.add(String.valueOf(String.valueOf(barre)+"€ et +"));
+            for (int j = 0; j < valeur.size(); j++) {
+                if(valeur.get(j)<barre2){
+                    nbBarre1=nbBarre1 +1;
+                }else if(valeur.get(j)<barre3){
+                    nbBarre2=nbBarre2 +1;
+                }else if(valeur.get(j)<barre4){
+                    nbBarre3=nbBarre3 +1;
+                }else if(valeur.get(j)<barre5){
+                    nbBarre4=nbBarre4 +1;
+                }else if(valeur.get(j)<barre6){
+                    nbBarre5=nbBarre5 +1;
+                }else{
+                    nbBarre6=nbBarre6 +1;
+                }
+
+            }
+
+
+
+            barNbValeur.add(nbBarre1);
+            barNbValeur.add(nbBarre2);
+            barNbValeur.add(nbBarre3);
+            barNbValeur.add(nbBarre4);
+            barNbValeur.add(nbBarre5);
+            barNbValeur.add(nbBarre6);
+            Log.i("CCCCCCCCCCC", String.valueOf(barEtendue));
+            Log.i("CCCCCCCCCCC", String.valueOf(barNbValeur));
+            Log.i("CCCCCCCCCCC", String.valueOf(etendue));
+        }
     }
 
     public void goStatistiques(View view) { //Changement d'état d'un bouton sélectionné/déselectionnée
-        Intent i = new Intent(); /* Intent de type direct */
-        i.setClass(this, Statistiques.class);
-        //PUT LES VALEURS NECESSAIRES POUR FAIRE LES STATS
-        this.startActivity(i); /* Poussé sur le bus */
+        if(arlText.size()>0) {
+            Intent i = new Intent(); /* Intent de type direct */
+            i.setClass(this, Statistiques.class);
+            Bundle sendEtendue = new Bundle();
+            sendEtendue.putSerializable("Etendue", barEtendue);
+            i.putExtras(sendEtendue);
+            Bundle sendNbValeur = new Bundle();
+            sendNbValeur.putSerializable("nbValeur", barNbValeur);
+            i.putExtras(sendNbValeur);
+            //PUT LES VALEURS NECESSAIRES POUR FAIRE LES STATS
+            this.startActivity(i); /* Poussé sur le bus */
+        }
+        else{
+            Toast.makeText(this,"Aucun bien trouvé, Statistiques indisponible",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
@@ -119,4 +187,78 @@ public class Resultat extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
     }
+
+
+    public int transfoZeroPremier(Float i){
+        String nbrChar = String.valueOf(i);
+        String  c1 = null;
+        if(i<10000){
+            c1="0";
+        }
+        else if(i<100000) {
+
+            c1=nbrChar.substring(0,1);
+            c1=c1+"0000";
+
+        }else if(i<1000000){
+            c1=nbrChar.substring(0,1);
+            c1=c1+"00000";
+        }else{
+            c1=nbrChar.substring(0,2);
+            c1=c1+"00000";
+        }
+        return  Integer.parseInt(c1);
+
+    }
+
+    public int transfoZerodernier(Float i){
+        String nbrChar = String.valueOf(i);
+        String  c1 = null;
+        int intermediaire= 0;
+
+        if(i<10000){
+            intermediaire=0;
+        }
+        else if(i<100000) {
+            c1=nbrChar.substring(0,1);
+            c1=c1+"0";
+            intermediaire = Integer.parseInt(c1)+10000;
+
+        }else if(i<1000000){
+            c1=nbrChar.substring(0,1);
+            c1=c1+"00000";
+            intermediaire = Integer.parseInt(c1)+100000;
+        }else{
+            c1=nbrChar.substring(0,2);
+            c1=c1+"00000";
+            intermediaire = Integer.parseInt(c1)+100000;
+        }
+        return  intermediaire;
+
+    }
+
+    public int transfoZeroEtendue(Float i){
+        String nbrChar = String.valueOf(i);
+        String  c1 = null;
+        if(i<10000){
+            c1="1000";
+        }
+        else if(i<100000) {
+            c1="10000";
+
+        }else if(i<1000000){
+            c1=nbrChar.substring(0,1);
+            c1=c1+"00000";
+        }else{
+            c1=nbrChar.substring(0,2);
+            c1=c1+"00000";
+        }
+        return  Integer.parseInt(c1);
+
+
+    }
+
 }
+
+
+
--- app/src/main/java/fr/univpau/quelpriximmo/Statistiques.java
+++ app/src/main/java/fr/univpau/quelpriximmo/Statistiques.java
@@ -1,16 +1,84 @@
 package fr.univpau.quelpriximmo;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Statistiques extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistiques);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        Bundle receiveEtendue = getIntent().getExtras();
+        ArrayList<String> stringEtendue = (ArrayList<String>)receiveEtendue.getSerializable("Etendue");
+        Log.i("BBBBBBBB", String.valueOf(stringEtendue));
+
+        Bundle receiveNbValeur = getIntent().getExtras();
+        ArrayList<Integer> nbValeur = (ArrayList<Integer>)receiveEtendue.getSerializable("nbValeur");
+        Log.i("BBBBBBBB", String.valueOf(nbValeur));
+
+        BarChart barChart = findViewById(R.id.fragment_verticalbarchart_chart);
+        ArrayList<BarEntry>barEntries = new ArrayList<BarEntry>();
+
+        barEntries.add(new BarEntry(0, nbValeur.get(0)));
+        barEntries.add(new BarEntry(1, nbValeur.get(1)));
+        barEntries.add(new BarEntry(2, nbValeur.get(2)));
+        barEntries.add(new BarEntry(3, nbValeur.get(3)));
+        barEntries.add(new BarEntry(4, nbValeur.get(4)));
+        barEntries.add(new BarEntry(5, nbValeur.get(5)));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Nombre de bien vendus dans la tranche de prix");
+        barDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        //        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setHighlightEnabled(true);
+        barDataSet.setHighLightColor(Color.RED);
+        barDataSet.setColors(new int[]{Color.parseColor("#FF7F50")});
+        barDataSet.setDrawValues(false);
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.getDescription().setTextSize(12);
+        barChart.getDescription().setText("Nb de ventes par tranche");
+        barChart.setDrawMarkers(true);
+        barChart.getXAxis().setPosition(XAxis.XAxisPosition.TOP);
+
+        ArrayList<String> labels = new ArrayList<String> ();
+
+        labels.add( stringEtendue.get(0));
+        labels.add( stringEtendue.get(1));
+        labels.add( stringEtendue.get(2));
+        labels.add( stringEtendue.get(3));
+        labels.add( stringEtendue.get(4));
+        labels.add( stringEtendue.get(5));
+
+
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(labels));
+        barChart.animateY(1000);
+
+        barChart.setData(barData);
     }
 
     @Override
@@ -18,4 +86,5 @@ public class Statistiques extends AppCompatActivity {
         super.onBackPressed();
         return true;
     }
+
 }
